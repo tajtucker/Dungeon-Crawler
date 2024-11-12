a7ae6e8d2de561125fb88b1dbc223012f645e815
Taj Tucker <tucker8258@gmail.com>
2024-11-12 11:11:59 -0600

    Update dungeon-crawler.py

--- dungeon-crawler.py
+++ dungeon-crawler.py
@@ -1,37 +1,28 @@
 #Varaibles
-dungeon_crpyt = 'crypt'
-dungeon_egypt = 'egypt'
-dungeon_woods = 'wooded'
 game_playing = True
 
-#Beginning Function
-def beginning():
+#Player's Name/Intro
+name = str(input(f"Hello Brave Adventurer! Please enter your name: "))
+print()
+players_name = name.capitalize()
+print(f"Hello " + players_name + "! Welcome to Dungeon Crawler! Your goal is to escape three dangerous dungeons ahead of you. Be ready to use you brain, as well as your brawn!")
+print()
+
+#Dungeon Choosing
+def Dungeon_Choosing():
+    global chosen_dungeon
     while game_playing:
-        name = str(input("Hello Brave Adventurer! Please enter your name: "))
-        players_name = name.capitalize()
-        print("\fHello " + players_name + "! Welcome to Dungeon Crawler! Your goal is to escape three dangerous dungeons ahead of you. Be ready to use you brain, as well as your brawn!")
-        print()
+        chosen_dungeon = input(f"Carefully choose a dungeon from the list - (Crypt Dungeon, Desert Dungeon, or Wooded Dungeon): ").capitalize()
+        
+        if chosen_dungeon == 'Crypt' or chosen_dungeon == 'Desert' or chosen_dungeon == 'Wooded':
+            print()
+            print(f"Prepare to enter the {chosen_dungeon} Dungeon! ")
+            break 
+        else:
+            print()
+            print(f"That is not an option. Please type the name of the Dungeon. (Exclude the word Dungeon)")
+            print()
     
-        dungeon = input("\fCarefully type the dungeon's name that you wish to enter: Crypt, Egypt, or Wooded: ").lower()
-    
-        if dungeon != dungeon_crpyt or dungeon_egypt or dungeon_woods:
-            print("Please enter a dungeon from the list.")
-        elif dungeon == ('crypt'):
-            print("\fYou have chosen the Crypt Dungeon! Strap in as you enter your impending doom!")
-        elif dungeon == ('egypt'):
-            print("\fYou have chosen the Egypt Dungeon! Strap in as you enter your impending doom!")
-        elif dungeon == ('wooded'):
-            print("\fYou have chosen the Wooded Dungeon! Strap in as you enter your impending doom!")
         
 
-    
-    chosen_dungeon = dungeon.capitalize()
-    print("\fYou have chosen the " + chosen_dungeon + " Dungeon! Strap in as you enter your impending doom!")
-
-#Main
-def main():
-    while game_playing == True:
-        break
-
-
-beginning()
+Dungeon_Choosing()
