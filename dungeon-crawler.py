99cd85025aed893d3a3a133e4026ddef29c82e3d
Taj Tucker <tucker8258@gmail.com>
2024-11-13 14:46:42 -0600

    Update dungeon-crawler.py

--- dungeon-crawler.py
+++ dungeon-crawler.py
@@ -1,33 +1,12 @@
 import random
 #Varaibles
 game_playing = True
-all_riddles = {"riddle1" : "Riddle: I am alive but I am not human. I was not born but I was made. My body was created from wood. My best friend is a cricket. What am I? " , 
-               "riddle2" : 2 , 
-               "riddle3" : 3 , 
-               "riddle4" : 4 , 
-               "riddle5" : 5
-               }
-all_riddle_answers = {"riddle1" : "pinocchio",
-                      "riddle2" : 2,
-                      "riddle3" : 3,
-                      "riddle4" : 4,
-                      "riddle5" : 5
-                      }
-all_monsters = {"Zobmie" : "You have encountered a Zombie! What should you do?",
-                "Skeleton" : 2,
-                "Vampire" : 3,
-                "Troll" : 4,
-                "Ghost" : 5
-                }
-all_monsters_answers = {"Zobmie" : 1,
-                        "Skeleton" : 2,
-                        "Vampire" : 3,
-                        "Troll" : 4,
-                        "Ghost" : 5}
-all_puzzles = {}
-all_possible_encounters = [all_riddles , 
-                           all_monsters ,
-                           all_puzzles ,]
+crypt = {"riddle":"I slip through the murk, silent and quick, With scales and teeth, sharp and slick. What am I?" , 
+"puzzle" : "In a swamp, there are 3 frogs on a lily pad. Each frog catches 2 flies. How many flies are caught in total?" ,
+"monster" : "You encounter a Slime! Would you like to run or fight?"
+}
+
+desert = {"riddle":}
 
 #Player's Name/Intro
 name = str(input(f"Hello Brave Adventurer! Please enter your name: "))
@@ -51,15 +30,7 @@ def dungeon_choosing():
             print(f"That is not an option. Please type the name of the Dungeon. (Exclude the word Dungeon)")
             print()
 
-#Dungeon Encounters
-def dungeon_prompts():
-   global riddle
-   global answer
-   riddle = random.choice(all_possible_encounters)
-   answer = all_possible_encounters.keys()
-   print(answer)
+
    
 
-
 dungeon_choosing()
-dungeon_prompts()
